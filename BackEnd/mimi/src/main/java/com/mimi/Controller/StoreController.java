89f2393fbef84a775862597bd37d50ca9b8b2e8d
김수현 <kimsoo5133@gmail.com>
2020-10-29 15:10:46 +0900

    data add

--- BackEnd/mimi/src/main/java/com/mimi/Controller/StoreController.java
+++ BackEnd/mimi/src/main/java/com/mimi/Controller/StoreController.java
@@ -1,8 +1,6 @@
 package com.mimi.Controller;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,10 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.mimi.Dto.Recommand;
 import com.mimi.Dto.Store;
 import com.mimi.Service.StoreService;
-
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -25,10 +21,9 @@ public class StoreController {
 
 	@Autowired
 	StoreService storeService;
-	
-	
+
 	@GetMapping()
-	public ResponseEntity<?> test(){
+	public ResponseEntity<?> test() {
 		List<Store> list = storeService.findAll();
 		System.out.println("test");
 		for (int i = 0; i < list.size(); i++) {
@@ -36,34 +31,17 @@ public class StoreController {
 		}
 		return new ResponseEntity<List<Store>>(list, HttpStatus.OK);
 	}
-	
-	@GetMapping("/recom")
-	public ResponseEntity<?> recommand_save() throws InterruptedException, IOException{
-		List<Recommand> ls = storeService.recom(" dany","역삼");
-		for(int i = 0;i<10;i++) {
-			System.out.println(ls.get(i));
-		}
-		return new ResponseEntity<List<Recommand>>(ls, HttpStatus.OK);
-	}
-	
-	
-	@GetMapping("/recommand")
-	public ResponseEntity<?> recommand() throws InterruptedException, IOException{
+
+	@GetMapping("surbey")
+	public ResponseEntity<?> surbey() {
 		List<Store> list = storeService.findAll();
-		System.out.println("recommand start.....");
-		String path = "C:/test/MongoConnect.py";
-		ProcessBuilder processBuilder = new ProcessBuilder("python", path);
-		processBuilder.redirectErrorStream(true);
-		Process p = processBuilder.start();
-		p.waitFor();
-		BufferedReader bfr = new BufferedReader(new InputStreamReader(p.getInputStream()));
-		String line = "";
-		while ((line = bfr.readLine()) != null) {
-		System.out.println(line);
+		List<Store> ret = new LinkedList<Store>();
+		for (int i = 0; i < 5; i++) {
+			int ran = (int) (Math.random() * 9999999 % list.size());
+			ret.add(list.get(ran));
 		}
-		
-		
-		System.out.println("recommand end.....");
-		return new ResponseEntity<List<Store>>(list, HttpStatus.OK);
+		return new ResponseEntity<List<Store>>(ret, HttpStatus.OK);
+
 	}
+
 }
