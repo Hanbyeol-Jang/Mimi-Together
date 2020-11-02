578941a4677a4aa0cc5621007af60d6a65a2c463
이대길 <eedaegil@yonsei.ac.kr>
2020-11-02 22:29:36 +0900

    Create Notification Channel

---
+++ App/app/src/main/java/com/chd/mimitogether/ApplicationContext.kt
@@ -0,0 +1,34 @@
+package com.chd.mimitogether
+
+import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+
+class ApplicationContext : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val id = getString(R.string.party_notification_channel_id)
+            val name = getString(R.string.party_notification_channel_name)
+            val descriptionText = getString(R.string.party_notification_channel_desc)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+}
