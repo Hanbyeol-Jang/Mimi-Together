22a832b65ce05058294009fb2147dc662c7a075c
이대길 <eedaegil@yonsei.ac.kr>
2020-11-03 17:11:09 +0900

    Modify MainActivity using FrameLayout

--- App/app/src/main/java/com/chd/mimitogether/MainActivity.kt
+++ App/app/src/main/java/com/chd/mimitogether/MainActivity.kt
@@ -2,29 +2,43 @@ package com.chd.mimitogether
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.chd.mimitogether.ui.dashboard.DashboardFragment
+import com.chd.mimitogether.ui.home.HomeFragment
+import com.chd.mimitogether.ui.notifications.NotificationsFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    private val dashboardFragment: DashboardFragment = DashboardFragment()
+    private val homeFragment: HomeFragment = HomeFragment()
+    private val notificationsFragment: NotificationsFragment = NotificationsFragment()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss()
+
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
-        val intent = Intent(this, LinkActivity::class.java)
-        startActivity(intent)
+        navView.setOnNavigationItemSelectedListener {
+            val transaction: FragmentTransaction = fragmentManager.beginTransaction()
+            Log.d("myLog", it.itemId.toString())
+            when(it.itemId){
+                R.id.navigation_home -> transaction.replace(R.id.frame_layout, homeFragment).commitAllowingStateLoss()
+                R.id.navigation_dashboard -> transaction.replace(R.id.frame_layout, dashboardFragment).commitAllowingStateLoss()
+                R.id.navigation_notifications -> transaction.replace(R.id.frame_layout, notificationsFragment).commitAllowingStateLoss()
+            }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
