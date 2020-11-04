3a56555205ff70454ca423f71522841fef968841
김수현 <kimsoo5133@gmail.com>
2020-11-04 17:30:09 +0900

    party location

--- BackEnd/mimi/src/main/java/com/mimi/Controller/PartyController.java
+++ BackEnd/mimi/src/main/java/com/mimi/Controller/PartyController.java
@@ -52,6 +52,7 @@ public class PartyController {
 			User user = userService.getUserinfo(partyReq.getUserID()).get();
 			Party party = partyService.createParty(partyReq);
 			List<String> list= user.getPartyList();
+			
 			list.add(party.getId());
 			user.setPartyList(list);
 			userService.update(user);
